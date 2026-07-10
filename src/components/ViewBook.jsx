351e056a3aef7757de3dc48d5adfcc82e5f0e0fa
arya631 <aryasreeragps@gmail.com>
2026-07-10 11:37:14 +0530

    view changed

--- src/components/ViewBook.jsx
+++ src/components/ViewBook.jsx
@@ -1,147 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
-function ViewBook() {
+const ViewBook = () => {
+  const [data, changeData] = useState([
+    {
+      book: "The Alchemist",
+      avathar: "https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg",
+      author: "Paulo Coelho",
+      price: "499"
+    },
+    {
+      book: "Atomic Habits",
+      avathar: "https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg",
+      author: "James Clear",
+      price: "599"
+    },
+    {
+      book: "Rich Dad Poor Dads",
+      avathar: "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL.jpg",
+      author: "Robert Kiyosaki",
+      price: "450"
+    },
+    {
+      book: "Think and Grow Rich",
+      avathar: "https://images-na.ssl-images-amazon.com/images/I/71UypkUjStL.jpg",
+      author: "Napoleon Hill",
+      price: "350"
+    }
+  ])
+
   return (
     <div>
       <NavBar />
 
-      <div className="container">
+      <div className="container mt-4">
         <div className="row">
-          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <div className="col-12">
             <div className="row g-3">
 
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <div className="card">
-                  <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg"
-                    className="card-img-top"
-                    alt="The Alchemist"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">The Alchemist</h5>
-                    <p className="card-text">
-                      <strong>Author:</strong> Paulo Coelho
-                    </p>
-                    <p className="card-text">
-                      <strong>Price:</strong> ₹499
-                    </p>
-                    <button className="btn btn-success">
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
+              {data.map((value, index) => {
+                return (
+                  <div
+                    className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6"
+                  >
+                    <div className="card h-100">
+                      <img
+                        src={value.avathar}
+                        className="card-img-top"
+                        alt={value.book}
+                      />
 
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <div className="card">
-                  <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg"
-                    className="card-img-top"
-                    alt="Atomic Habits"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">Atomic Habits</h5>
-                    <p className="card-text">
-                      <strong>Author:</strong> James Clear
-                    </p>
-                    <p className="card-text">
-                      <strong>Price:</strong> ₹599
-                    </p>
-                    <button className="btn btn-success">
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
+                      <div className="card-body">
+                        <h5 className="card-title">{value.book}</h5>
 
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <div className="card">
-                  <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL.jpg"
-                    className="card-img-top"
-                    alt="Rich Dad Poor Dad"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">Rich Dad Poor Dad</h5>
-                    <p className="card-text">
-                      <strong>Author:</strong> Robert Kiyosaki
-                    </p>
-                    <p className="card-text">
-                      <strong>Price:</strong> ₹450
-                    </p>
-                    <button className="btn btn-success">
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
+                        <p className="card-text">
+                          <strong>Author:</strong> {value.author}
+                        </p>
 
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <div className="card">
-                  <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/81l3rZK4lnL.jpg"
-                    className="card-img-top"
-                    alt="Ikigai"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">Ikigai</h5>
-                    <p className="card-text">
-                      <strong>Author:</strong> Héctor García
-                    </p>
-                    <p className="card-text">
-                      <strong>Price:</strong> ₹399
-                    </p>
-                    <button className="btn btn-success">
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
+                        <p className="card-text">
+                          <strong>Price:</strong> ₹{value.price}
+                        </p>
 
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <div className="card">
-                  <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/71UypkUjStL.jpg"
-                    className="card-img-top"
-                    alt="Think and Grow Rich"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">Think and Grow Rich</h5>
-                    <p className="card-text">
-                      <strong>Author:</strong> Napoleon Hill
-                    </p>
-                    <p className="card-text">
-                      <strong>Price:</strong> ₹350
-                    </p>
-                    <button className="btn btn-success">
-                      Add to Cart
-                    </button>
+                        <button className="btn btn-success">
+                          Add to Cart
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <div className="card">
-                  <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/71g2ednj0JL.jpg"
-                    className="card-img-top"
-                    alt="The Psychology of Money"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">The Psychology of Money</h5>
-                    <p className="card-text">
-                      <strong>Author:</strong> Morgan Housel
-                    </p>
-                    <p className="card-text">
-                      <strong>Price:</strong> ₹549
-                    </p>
-                    <button className="btn btn-success">
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
+                )
+              })}
 
             </div>
           </div>
